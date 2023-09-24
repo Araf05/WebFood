c47b222c1ff690285b7e334ad3e3d9df49b0c8c3
Araceli Fernández <noe_3006@live.com.ar>
2023-09-24 00:39:45 -0300

    probando Js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+    const reservaBtn = document.getElementById('reservaBtn');
+    reservaBtn.addEventListener('click', () => {
+        console.log('Gracias por reservar');
+    });
+} );
